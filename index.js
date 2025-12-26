bad8c48785344778b6a55c5affe661bcdf3a2551
Swastik <swastik.patro02@gmail.com>
2025-12-26 21:17:50 +0530

    added the frontend

--- index.js
+++ index.js
@@ -11,4 +11,6 @@ else{
     console.log("invalid command");
     process.exit(1)
 }
+//const ws = new WebSocket('') is used on the client-side (e.g., in a browser or a client-side JavaScript application) to establish a connection to a WebSocket server (1:12:19). This ws object represents the client's connection to the server.
 
+//wss = new WebSocketServer('') is used on the server-side (e.g., in a Node.js application) to create and manage WebSocket connections (0:52:40). This wss object is the WebSocket server itself, listening for incoming client connections.
